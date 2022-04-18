ee44562bc4fa7f69710ef0b334fba6d5862dc01b
diorcokorea <yknam@diorco.co.kr>
2022-04-18 14:41:30 +0800

    2nd

--- bezkoder-app/app/controllers/procedure.js
+++ bezkoder-app/app/controllers/procedure.js
@@ -1,5 +1,33 @@
 const db = require("../models");
-const crypto = require("../util/crypto");
+//const crypto = require("../util/crypto");
+
+var crypto = require("crypto");
+
+const algorithm = "aes-256-cbc";
+const ENCRYPTION_KEY = process.env.key; // or generate sample key Buffer.from('FoCKvdLslUuB4y3EZlKate7XGottHski1LmyqJHvUhs=', 'base64');
+const IV_LENGTH = 16;
+const KEY_LENGTH = 32;
+function encrypt(text) {
+  let key = Buffer.alloc(KEY_LENGTH);
+  let iv = Buffer.alloc(IV_LENGTH);
+  let temp = Buffer.from(ENCRYPTION_KEY);
+  temp.copy(key);
+  let cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf-8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
+}
+
+function decrypt(text) {
+  let key = Buffer.alloc(KEY_LENGTH);
+  let iv = Buffer.alloc(IV_LENGTH);
+  let temp = Buffer.from(ENCRYPTION_KEY);
+  temp.copy(key);
+  let decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(text, "base64", "utf-8");
+  decrypted += decipher.final("utf-8");
+  return decrypted;
+}
 
 exports.querytest = (req, res) => {
   db.sequelize
@@ -73,7 +101,7 @@ exports.couponuse = (req, res) => {
 };
 exports.couponcount = (req, res) => {
   console.log("body", req.body);
-  let decr = JSON.parse(crypto.decrypt(req.body.encrypt, "DIORCO20141111"));
+  let decr = JSON.parse(decrypt(req.body.encrypt));
 
   console.log("decr", decr);
 
--- bezkoder-app/server.js
+++ bezkoder-app/server.js
@@ -45,13 +45,40 @@ db.sequelize.sync({ force: false, alter: true });
 // });
 
 // simple route
-const cryp = require("./app/util/crypto");
+//const cryp = require("./app/util/crypto_old");
 
+var crypto = require("crypto");
+
+const algorithm = "aes-256-cbc";
+const ENCRYPTION_KEY = process.env.key; // or generate sample key Buffer.from('FoCKvdLslUuB4y3EZlKate7XGottHski1LmyqJHvUhs=', 'base64');
+const IV_LENGTH = 16;
+const KEY_LENGTH = 32;
+function encrypt(text) {
+  let key = Buffer.alloc(KEY_LENGTH);
+  let iv = Buffer.alloc(IV_LENGTH);
+  let temp = Buffer.from(ENCRYPTION_KEY);
+  temp.copy(key);
+  let cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf-8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
+}
+
+function decrypt(text) {
+  let key = Buffer.alloc(KEY_LENGTH);
+  let iv = Buffer.alloc(IV_LENGTH);
+  let temp = Buffer.from(ENCRYPTION_KEY);
+  temp.copy(key);
+  let decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(text, "base64", "utf-8");
+  decrypted += decipher.final("utf-8");
+  return decrypted;
+}
 app.get("/enc", (req, res) => {
   console.log(req.query);
   res.json({
     //decrypt: cryp.decrypt(),
-    encrypt: cryp.encrypt(JSON.stringify(req.query)),
+    encrypt: encrypt(JSON.stringify(req.query)),
   });
 });
 app.get("/dec", (req, res) => {
